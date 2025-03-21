8d9bcda9c1698103c9c4a2c7d500335d91a21a9f
LEILEI0628 <2239080538@qq.com>
2025-03-21 19:23:46 +0800

    add k8s mysql/redis

--- redbook/internal/web/init_web.go
+++ redbook/internal/web/init_web.go
@@ -22,9 +22,9 @@ func RegisterRouters(server *gin.Engine, db *gorm.DB) {
 	//server.Use(globalMiddleware.Session())     // 添加session（cookie中）（存储方式在方法中自定义）
 	//server.Use(middleware.NewLoginMiddlewareBuilder(). // 校验session
 	server.Use(middleware.NewLoginMiddlewareJWTBuilder(). // 校验JWT
-		IgnorePaths("/users/login"). // 链式调用，不同的server可定制（扩展性）
-		IgnorePaths("/users/signup").
-		Build()) // Builder模式为了解决复杂结构构建问题
+								IgnorePaths("/users/login"). // 链式调用，不同的server可定制（扩展性）
+								IgnorePaths("/users/signup").
+								Build()) // Builder模式为了解决复杂结构构建问题
 
 	initUserRouters(server, db).RegisterUserRouters()
 
