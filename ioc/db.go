7b4c7cbc37d48e4ea2fc94ad749c2477636e677f
LEILEI0628 <2239080538@qq.com>
2025-04-12 18:47:53 +0800

    add viper Config

--- ioc/db.go
+++ ioc/db.go
@@ -1,13 +1,28 @@
 package ioc
 
 import (
-	"github.com/LEILEI0628/GoWeb/config"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
+	type Config struct {
+		DSN string `yaml:"dsn"` // ？此处DSN必须为大写
+	}
+	var cfg Config
+	//var cfg Config = Config{DSN: "root:root@tcp(localhost:3306)/goweb"} // 设置默认值的方法2
+	//dsn := viper.GetString("db.mysql.dsn")
+	// 使用UnmarshalKey解析"db.mysql"时配置文件必须为树状结构：
+	// db:
+	//  mysql:
+	//   dsn: ""
+	// 不能写成db.mysql.dsn: ""的形式（使用配置文件和Remote方式时层级似乎不同，建议直接写成全树状结构）
+	err := viper.UnmarshalKey("db.mysql", &cfg) // 需要对cfg进行修改，传指针
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(mysql.Open(cfg.DSN))
 	if err != nil {
 		// panic相当于整个goroutine结束
 		// panic只会出现在初始化的过程中（一旦初始化出错，就没必要启动了）
--- ioc/redis.go
+++ ioc/redis.go
@@ -1,13 +1,23 @@
 package ioc
 
 import (
-	"github.com/LEILEI0628/GoWeb/config"
+	"fmt"
 	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
 )
 
 func InitRedis() redis.Cmdable {
+	type Config struct {
+		ADDR string `yaml:"addr"`
+	}
+	var cfg Config
+	//addr := viper.GetString("db.redis.addr")
+	err := viper.UnmarshalKey("db.redis", &cfg)
+	if err != nil {
+		panic(fmt.Errorf("初始化Redis失败：%v\n", err))
+	}
 	return redis.NewClient(&redis.Options{
-		Addr:     config.Config.Redis.Addr,
+		Addr:     cfg.ADDR,
 		Password: "",
 		DB:       0,
 	})
